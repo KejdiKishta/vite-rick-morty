d286f7280e82a35bf0ec85de3eb22755e4fe4218
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-06 15:44:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!-- *Descrizione:
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Al caricamento della pagina, effettuate una chiama ajax all'API di Rick and Morty:
+https://rickandmortyapi.com/api/character
+Potete consultare la documentazione completa di API qui: https://rickandmortyapi.com/documentation -->
+<!-- *Bonus:
+Creare un componente loader da visualizzare fintantoché i risultati non sono pronti. -->
+
+<script>
+export default {
+  components: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
