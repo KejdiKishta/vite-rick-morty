d86ac3daa6d12364ef8942958a628b39e18a2cd9
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-06 17:53:01 +0200

    add: bg, favicon and box-shadow

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -14,7 +14,7 @@ export default {
 <template>
     <div class="container m-auto">
         <div class="row justify-content-around">
-            <div class="col col-sm-6 col-md-4 col-lg-3" v-for="character in characters">
+            <div class="col-sm-6 col-md-4 col-lg-3" v-for="character in characters">
                 <Card :cardInfo="character" />
             </div>
         </div>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,15 +7,17 @@ export default {
 </script>
 
 <template>
-    <div class="card border-0">
-        <img :src="cardInfo.image" class="card-img-top rounded-circle" :alt="cardInfo.name">
+    <div class="card border-0 bg-transparent">
+        <img :src="cardInfo.image" class="card-img-top rounded-circle ms_shadow" :alt="cardInfo.name">
         <div class="card-body">
-            <p class="card-text fs-2 text-center">{{ cardInfo.name }}</p>
+            <p class="card-text fs-4 text-center ms_height">{{ cardInfo.name }}</p>
             <p class="card-text text-center">{{ cardInfo.status }} <br> {{ cardInfo.species }}</p>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-
+.ms_shadow {
+    box-shadow: 0 0 8px#888888;
+}
 </style>
