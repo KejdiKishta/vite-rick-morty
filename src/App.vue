a86b6e7130fc071364648aad18b87c63800d754a
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-07 16:10:14 +0200

    add: from milestone 1 to milestone 4

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,40 @@ Potete consultare la documentazione completa di API qui: https://rickandmortyapi
 <!-- *Bonus:
 Creare un componente loader da visualizzare fintantoché i risultati non sono pronti. -->
 
+<!-- *Descrizione:
+Continuate a lavorare nella stessa repo di ieri e aggiungete una select per filtrare i risultati in base al status:  alive, dead, unknown.
+Quando l'utente seleziona un valore dalla lista, viene effettuata una chiamata alle API con lo status corrispondente. -->
+<!-- *Come dovete procedere:
+?fatto
+1. Create il file dello store globale per salvare la variabile del selectedStatus
+2. Creare il componente AppSearch dove dovete inserire il select con delle options: alive, dead, unknown (create gli options dinamicamente tramite v-for)
+3. Inserire il componente AppSearch nel App.vue
+4. Create evento al cambiamento del valore del select in AppSearch. In App.vue inserite ascolto dell'evento e eseguite console.log per verificare se l'evento funzioni.
+?da fare
+5. Collegare tramite v-model il select al selectedStatus dello store (nel componente AppSearch)
+6. Nell App.vue importare lo store e in base al valore del selectedStatus fare la chiamata api. Nel then aggiornare i la lista di personaggi. -->
+<!-- *Bonus:
+1. Fare refactorin dell'app, inserendo la lista dei personaggi nello store globale
+2. Creare un componente che mostri il numero totale di risultati ottenuti. -->
+
 <script>
 // import componenti
+import { store } from "./store.js";
 import axios from 'axios';
 import AppCards from './components/AppCards.vue';
+import AppSearch from './components/AppSearch.vue';
 
 export default {
   components: {
-    AppCards
+    AppCards,
+    AppSearch,
   },
 
   data() {
     return {
       // array di destinazione axios
       characters: [],
+      store
     }
   },
 
@@ -31,12 +51,20 @@ export default {
       this.characters = resp.data.results;
       console.log(this.characters);
     })
+  },
+
+  methods: {
+    logStatus: function () {
+      console.log(store.selectedStatus);
+    }
   }
 }
 </script>
 
 <template>
   <h1 class="text-center py-5">Rick and Morty App</h1>
+
+  <AppSearch @filter="logStatus"/>
 
   <AppCards :characters="characters"/>
 </template>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,28 @@
+<script>
+import { store } from "../store.js";
+
+export default {
+    data() {
+        return {
+            store,
+            options: ["All", "Alive", "Dead", "Unknown"],
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <div class="container pb-5">
+        <label for="status" class="px-2">Filter by status:</label>
+        <select name="status" id="status" @change="$emit('filter')" v-model="store.selectedStatus">
+            <option :value="item" v-for="item in options">{{ item }}</option>
+        </select>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
